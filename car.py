76e79c2de80b2ae2c80b0b097205194a7023147e
Miguel Franjul <144841890+LeviathanGamez@users.noreply.github.com>
2026-03-19 12:40:19 -0400

    removed ray function

--- car.py
+++ car.py
@@ -13,6 +13,11 @@ class Car():
         self.X = x
         self.Y = y 
         self.angle = random.uniform(0, 2 * math.pi)
+        self.random = random.randint(1,5)
+        self.sprite = pygame.image.load(f"sprites/car{self.random}.png").convert_alpha()
+        self.sprite = pygame.transform.scale(self.sprite, (self.sprite.get_width() / 4*ZOOM, self.sprite.get_height() / 4*ZOOM))
+        self.rect = self.sprite.get_rect(center=(self.X, self.Y))
+        self.rotated_sprite = pygame.transform.rotate(self.sprite, self.angle)
         self.pos = pygame.Vector2(self.X, self.Y)
         self.acceleration = pygame.Vector2()
         self.velocity = pygame.Vector2()
@@ -43,7 +48,7 @@ class Car():
             self.path.popleft()
             return
 
-        direction = current_checkpoint.vector_from(self.pos.x, self.pos.y);
+        direction = current_checkpoint.vector_from(self.pos.x, self.pos.y)
 
         self.acceleration = ACCELERATION * direction
         self.acceleration += self.velocity * FRICTION
